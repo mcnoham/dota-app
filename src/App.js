85051784df83645970a34182819f6068478321f7
mcnoham <mikemcwfb@gmail.com>
2021-07-22 18:28:29 -0500

    yep

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import Button from "./components/Button";
+import React, { useState, Component } from 'react'
 
 function App() {
+  const [match, setMatch] = useState([]);
+
+  componentDidMount() {
+    fetch('https://api.opendota.com/api/matches/6099490856')
+      .then(response => response.json())
+      .then(data =>  console.log(data));
+  }
+  
   return (
-    
+    <>
+      Dota App
+      <div>
+        <p>{JSON.stringify(match)}</p>
+      </div>
+      <Button/>
+    </>
   );
 }
 
