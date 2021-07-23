353e627fb412a8a0c11efe07d2d1257e1bf92ef3
mcnoham <mikemcwfb@gmail.com>
2021-07-22 21:11:19 -0500

    hero names are now showing on scoreboard component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import Button from "./components/Button";
+import Scoreboard from "./components/Scoreboard";
 import React, { useState, useEffect } from 'react'
+import Heroes from './Heroes.json'
+import { pick } from "lodash";
 
 function App() {
+  const [ID, setID] = useState('')
   const [match, setMatch] = useState([])
   const [heroes, setHeroes] = useState([])
   const [players, setPlayers] = useState([])
 
-  useEffect(() => {
-    fetch('https://api.opendota.com/api/matches/6099490856')
-    .then(response => response.json())
-    .then(data =>  organizeMatch(data));
-  })
+  var endpoint = 'https://api.opendota.com/api/matches/' + ID
 
-  const organizeMatch = (data) =>{
-    setMatch(data)
-    setPlayers(data.players)
+  const getMatch = (evt) => {
+    console.log(endpoint)
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data =>  {
+      setMatch(data)
+      setPlayers(data.players)
+      console.log(players)
+    });
+    evt.preventDefault();
   }
+  
+  // const organizeMatch = (evt) =>{
+  //   getMatch()
+  //   setPlayers(match.players)
+  //   console.log(players)
+  //   evt.preventDefault();
+  // }
   
   return (
     <>
       Dota App
       <div>
-        <p>{JSON.stringify(match)}</p>
+      <form onSubmit={getMatch}>
+          <label >
+              Input Match id:
+              <input type='text' input={ID} onChange={e => setID(e.target.value)}/>
+          </label>
+          <input className='p-3 bg-blue-500 hover:bg-blue-300 rounded-md' type='submit'></input>
+          {/* <p>{JSON.stringify(players)}</p> */}
+        </form>
+        
       </div>
+      <Scoreboard picks = {players}/>
       <Button/>
     </>
   );
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Heroes from '../Heroes.json'
 
-function Scoreboard() {
+function Scoreboard({picks}) {
     return (
         <div>
-            
+            <div>{picks.map((x, index) => <div key={index}>{x.player_slot < 5? Heroes[x.hero_id].localized_name : null}</div>)}</div>
+            <div>{picks.map((x, index) => <div key={index}>{x.player_slot > 127? Heroes[x.hero_id].localized_name : null}</div>)}</div>
         </div>
     )
 }
