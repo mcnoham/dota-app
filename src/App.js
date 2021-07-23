4e3aeaa0e6d854bc080a439af69be116c074c314
mcnoham <mikemcwfb@gmail.com>
2021-07-22 20:24:06 -0500

    organizing opendota api call

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import Button from "./components/Button";
-import React, { useState, Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  const [match, setMatch] = useState([]);
+  const [match, setMatch] = useState([])
+  const [heroes, setHeroes] = useState([])
+  const [players, setPlayers] = useState([])
 
-  
-
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://api.opendota.com/api/matches/6099490856')
-      .then(response => response.json())
-      .then(data =>  console.log(data));
+    .then(response => response.json())
+    .then(data =>  organizeMatch(data));
+  })
+
+  const organizeMatch = (data) =>{
+    setMatch(data)
+    setPlayers(data.players)
   }
   
   return (
